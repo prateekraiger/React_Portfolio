c8ca011a2a77d96faff6e0cbaa33120439adf192
Pratik <prateekraiger123@gmail.com>
2024-12-05 16:37:17 +0530

    new background

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ const App = () => {
       selection:bg-cyan-300  selection:text-cyan-900"
     >
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+        <div class="relative h-full w-full bg-black">
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        </div>
       </div>
 
       <div className="container mx-auto px-8">
