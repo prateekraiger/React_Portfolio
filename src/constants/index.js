b02a3759aa19b959ef829d32de95c60f8c5498fd
Pratik <167973139+prateekraiger@users.noreply.github.com>
2024-12-24 18:03:14 +0530

    updated context

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,16 +4,16 @@ import project3 from "../assets/projects/project3.png";
 import project4 from "../assets/projects/project4.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and
-                              scalable web applications. With 5 years of hands-on experience,
+                              scalable web applications. With 1 year of hands-on experience,
                               I have honed my skills in front-end technologies like React and Next.js,
-                               as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB.
+                               as well as back-end technologies like Node.js, MySQL, and MongoDB.
                                 My goal is to leverage my expertise to create innovative solutions that
                                 drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient
-                           and user-friendly web applications. With 5 years of professional experience,
+                           and user-friendly web applications. With 1 year of professional experience,
                            I have worked with a variety of technologies,
-                           including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB.
+                           including React, Next.js, Node.js, MySQL, and MongoDB.
                             My journey in web development began with a deep curiosity for how things work,
                             and it has evolved into a career where I continuously strive to learn and
                             adapt to new challenges. I thrive in collaborative environments and enjoy solving
