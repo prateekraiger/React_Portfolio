74664617838227d043c2d6dde33518fb7193c37a
Pratik <prateekraiger123@gmail.com>
2025-03-31 15:20:44 +0530

    updated image and hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/image1.png";
 import { motion, transform } from "framer-motion";
 
 const container = (delay) => ({
